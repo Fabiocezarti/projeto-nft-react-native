5a873fb1b0ccc9e0d6a1189e6b04dfd19d11c222
Fabio Cezar Brito <fabiocezarti@gmail.com>
2022-07-10 21:40:29 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# projeto-nft-react-native
- Este é um repositório de código para prática em React Native.
+<div align="center">
+    <img src="./assets/images/logo1.png" width="75">
+    <h1>
+        Fabio Cezar
+    </h1>
+    <h3>Jovem apaixonado por programação e tecnologias.</h3>
+    <p>A NFT Prime é um aplicativo mobile para android e ios de leilões NFT onde vários criadores podem leiloar suas obras digitais a um lance mínimo que ache justo. É estabelecido um tempo de duração da oferta do Leilão para que seja feito os lances. Desenvolvi usando as seguintes tecnologias: React Native, Expo e Node.js e como usar o Expo CLI para implantar e publicar meu aplicativo.</p>
+</div>
+
+<img src="./assets/images/portfolio.png">
---
+++ constants/dummy.js
@@ -0,0 +1,202 @@
+import assets from "./assets";
+
+const NFTData = [
+  {
+    id: "NFT-01",
+    name: "Bored Ape Comunist",
+    creator: "Vladimir Putin",
+    price: 4.25,
+    description:
+      "O expressionismo primata do autor foi diretamente influenciado pelo comunismo. Putin canalizou isso para a sua produção e imagem gestual.",
+    image: assets.nft01,
+    bids: [
+      {
+        id: "BID-11",
+        name: "Neymar Jr",
+        price: 4.25,
+        image: assets.person02,
+        date: "18 de Junho de 2022 às 12:10",
+      },
+      {
+        id: "BID-12",
+        name: "Anitta",
+        price: 4.15,
+        image: assets.person03,
+        date: "12 de Junho de 2022 às 13:50",
+      },
+      {
+        id: "BID-13",
+        name: "Gusttavo Lima",
+        price: 4.12,
+        image: assets.person04,
+        date: "31 de Maio de 2022 às 15:30",
+      },
+    ],
+  },
+  {
+    id: "NFT-02",
+    name: "Human One",
+    creator: "Thomas Shelby",
+    price: 7.25,
+    description:
+      "Dentre todas a prisões, a pior é a que você não vê. Sentimentos Interligados de uma releitura do cubismo, O autor buscou ser frio e calculista em expressar a prisão que todos nós estamos.",
+    image: assets.nft02,
+    bids: [
+      {
+        id: "BID-21",
+        name: "Gusttavo Lima",
+        price: 7.05,
+        image: assets.person04,
+        date: "21 de Junho de 2022 às 08:30",
+      },
+    ],
+  },
+  {
+    id: "NFT-03",
+    name: "The Merge",
+    creator: "Gon Freecss",
+    price: 95.25,
+    description:
+      "Inspirado no conceito das Esferas de Dyson, o criador representa em sua NFT o tamanho colossal que seria uma hipotética esfera perto de nossa estrela...O Sol.",
+    image: assets.nft03,
+    bids: [
+      {
+        id: "BID-31",
+        name: "Neymar Jr",
+        price: 95.25,
+        image: assets.person02,
+        date: "22 de Junho de 2022 às 23:07",
+      },
+      {
+        id: "BID-32",
+        name: "Anitta",
+        price: 95.15,
+        image: assets.person03,
+        date: "21 de Junho de 2022 às 18:42",
+      },
+    ],
+  },
+  {
+    id: "NFT-04",
+    name: "A Coin for the Ferryman (XCopy)",
+    creator: "Walter White",
+    price: 54.25,
+    description:
+      "Uma arte quase abstrata, o autor quis apresentar uma paródia de uma freira que sempre quis usar óculos da cor pink, mas a sua religião não permitia...Então ele sonhou que ela as usava.",
+    image: assets.nft04,
+    bids: [
+      {
+        id: "BID-41",
+        name: "Neymar Jr",
+        price: 56.25,
+        image: assets.person02,
+        date: "21 de Junho de 2022 às 22:08",
+      },
+      {
+        id: "BID-42",
+        name: "Anitta",
+        price: 55.25,
+        image: assets.person03,
+        date: "12 de Junho de 2022 às 12:50",
+      },
+      {
+        id: "BID-43",
+        name: "Gusttavo Lima",
+        price: 55.15,
+        image: assets.person04,
+        date: "11 de Junho de 2022 às 11:23",
+      },
+      {
+        id: "BID-44",
+        name: "Neymar Jr",
+        price: 54.15,
+        image: assets.person02,
+        date: "10 de Junho de 2022 às 06:09",
+      },
+    ],
+  },
+  {
+    id: "NFT-05",
+    name: "Ocean Front",
+    creator: "Saul Goodman",
+    price: 10.25,
+    description:
+      "Uma peculiar representação de uma residência marítima toda feita de meios de transportes, containers e de outros tipos de casas sobre rodas. Neste caso se você quiser uma carona...só se for de lancha.",
+    image: assets.nft05,
+    bids: [
+      {
+        id: "BID-51",
+        name: "Neymar Jr",
+        price: 10.25,
+        image: assets.person02,
+        date: "17 de Junho de 2022 às 14:15",
+      },
+    ],
+  },
+  {
+    id: "NFT-06",
+    name: "Everdays: The First 5000 Days",
+    creator: "Thomas Anderson",
+    price: 20.25,
+    description:
+      "Uma NFT que é essencialmente uma colagem de 5.000 peças de seu trabalho. Ela se diferencia de outras obras por ser uma obra física e digital, que pode (e deve) ser alterada por seu criador no futuro.",
+    image: assets.nft06,
+    bids: [
+      {
+        id: "BID-61",
+        name: "Neymar Jr",
+        price: 20.25,
+        image: assets.person02,
+        date: "19 de Junho de 2022 às 17:33",
+      },
+      {
+        id: "BID-62",
+        name: "Anitta",
+        price: 20.15,
+        image: assets.person03,
+        date: "19 de Junho de 2022 às 11:10",
+      },
+      {
+        id: "BID-63",
+        name: "Gusttavo Lima",
+        price: 20.05,
+        image: assets.person04,
+        date: "18 de Junho de 2022 às 11:10",
+      },
+      {
+        id: "BID-64",
+        name: "Neymar Jr",
+        price: 19.25,
+        image: assets.person02,
+        date: "16 de Junho de 2022 às 08:18",
+      },
+      {
+        id: "BID-65",
+        name: "Anitta",
+        price: 17.25,
+        image: assets.person03,
+        date: "15 de Junho de 2022 às 12:03",
+      },
+    ],
+  },
+  {
+    id: "NFT-07",
+    name: "Crossroad",
+    creator: "Humberto Guessinger",
+    price: 18.25,
+    description:
+      "Ele retrata pedestres passando ao lado de um corpo enorme, com ofensas e insultos escritos por toda parte de seu corpo.",
+    image: assets.nft07,
+    bids: [
+      {
+        id: "BID-63",
+        name: "Gusttavo Lima",
+        price: 18.25,
+        image: assets.person04,
+        date: "21 de Junho de 2022 às 09:10",
+      },
+    ],
+  },
+];
+
+export { NFTData };
